c446d98db92fc372641eecf57e6b732e6f28d921
Rhys Adams <rhys@therhys.co.uk>
2024-12-31 16:59:32 +0000

    Fix image sizes and improve compression script.

--- assets/images/convert.pl
+++ assets/images/convert.pl
@@ -1,7 +1,9 @@
+#!/usr/bin/env perl
+
 use warnings;
 use strict;
 
-my $quality = 50;
+my $quality = 20;
 
 for(<*.jpg>) {
     next if(/.*\.small.jpg/);
@@ -9,7 +11,7 @@ for(<*.jpg>) {
     if(/(.*).jpg/) {
         next if (-e "$1.small.jpg");
 
-        system("convert $1.jpg -auto-orient -compress JPEG -quality $quality $1.small.jpg");
+        system("convert $1.jpg -sampling-factor 4:2:0 -strip -quality $quality -interlace JPEG -colorspace RGB $1.small.jpg");
         unlink("$1.jpg");
     }
 }
