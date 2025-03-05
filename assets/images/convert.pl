94cf95aed2d10aad0e77b28e5c89af007b721db0
Rhys Adams <rhys@therhys.co.uk>
2025-03-05 11:11:36 +0000

    Change converter to webp.

--- assets/images/convert.pl
+++ assets/images/convert.pl
@@ -6,12 +6,13 @@ use strict;
 my $quality = 20;
 
 for(<*.jpg>) {
-    next if(/.*\.small.jpg/);
+    next if(/.*\.webp/);
 
     if(/(.*).jpg/) {
-        next if (-e "$1.small.jpg");
+        next if (-e "$1.webp");
 
-        system("convert $1.jpg -sampling-factor 4:2:0 -strip -quality $quality -interlace JPEG -colorspace RGB $1.small.jpg");
+        # system("convert $1.jpg -sampling-factor 4:2:0 -strip -quality $quality -interlace JPEG -colorspace RGB $1.small.jpg");
+        system("cwebp -size 200000 -mt -m 6 -q 50 $1.jpg -o $1.webp");
         unlink("$1.jpg");
     }
 }
